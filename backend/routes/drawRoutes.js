2862d55056d1379048bdec1b270f3f492732f582
romiali <rumialirome@gmail.com>
2025-09-15 00:51:34 +0500

    Update drawRoutes.js

--- backend/routes/drawRoutes.js
+++ backend/routes/drawRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { getDraws } = require('../controllers/drawController');
+const path = require('path');
+const { getDraws } = require(path.join(__dirname, '..', 'controllers', 'drawController'));
 
 // This is a public route, no protection needed
 router.get('/', getDraws);
