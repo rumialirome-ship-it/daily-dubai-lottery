c66184c743756804b60aad0e07f6956c94732f86
romiali <rumialirome@gmail.com>
2025-09-15 00:52:41 +0500

    Update healthRoutes.js

--- backend/routes/healthRoutes.js
+++ backend/routes/healthRoutes.js
@@ -1,1 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+const { checkHealth } = require(path.join(__dirname, '..', 'controllers', 'healthController'));
+
+// This is a public route for health checking
+router.get('/', checkHealth);
+
+module.exports = router;
 
