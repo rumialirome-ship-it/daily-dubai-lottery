3918bb5d681b4f30468c807cecd0489ec7246309
romiali <rumialirome@gmail.com>
2025-09-15 00:50:35 +0500

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { loginUser } = require('../controllers/authController');
+const path = require('path');
+const { loginUser } = require(path.join(__dirname, '..', 'controllers', 'authController'));
 
 router.post('/login', loginUser);
 
