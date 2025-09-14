457ddc458b33311e1e350afcc92a925930890451
romiali <rumialirome@gmail.com>
2025-09-14 19:23:41 +0500

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
       NODE_ENV: 'development',
       PORT: 5000,
       JWT_SECRET: 'ddl_secret_key_for_jwt_2024_dev',
-      DB_HOST: 'localhost',
+      DB_HOST: '127.0.0.1',
       DB_USER: 'ddl_user',
       DB_PASSWORD: 'password',
       DB_DATABASE: 'ddl_lottery'
@@ -18,7 +18,7 @@ module.exports = {
       NODE_ENV: 'production',
       PORT: 5000,
       JWT_SECRET: 'a_much_stronger_production_secret_for_ddl_app_!@#$', // This MUST be changed to a secure, random key for production
-      DB_HOST: 'localhost',
+      DB_HOST: '127.0.0.1',
       DB_USER: 'ddl_user',
       DB_PASSWORD: 'a_strong_production_password', // This MUST be changed to your production DB password
       DB_DATABASE: 'ddl_lottery'
