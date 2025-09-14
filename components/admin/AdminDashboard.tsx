642d789ef437b12cb0176e2a7e60539fc32ec94c
romiali <rumialirome@gmail.com>
2025-09-15 03:12:05 +0500

    Update AdminDashboard.tsx

--- components/admin/AdminDashboard.tsx
+++ components/admin/AdminDashboard.tsx
@@ -5,7 +5,6 @@ import LiveBettingMonitor from './LiveBettingMonitor.tsx';
 import SmartReporting from './AIReporting.tsx';
 import SmartBulkBetting from './AIBulkBetting.tsx';
 import ProfileSettings from './ProfileSettings.tsx';
-import DataManagement from './DataManagement.tsx';
 
 // Icons for the tabs
 const DrawIcon = () => <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}><path strokeLinecap="round" strokeLinejoin="round" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" /></svg>;
@@ -14,7 +13,6 @@ const BulkIcon = () => <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-
 const ClientsIcon = () => <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}><path strokeLinecap="round" strokeLinejoin="round" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" /></svg>;
 const ReportIcon = () => <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}><path strokeLinecap="round" strokeLinejoin="round" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" /></svg>;
 const ProfileIcon = () => <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}><path strokeLinecap="round" strokeLinejoin="round" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" /></svg>;
-const BackupIcon = () => <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}><path strokeLinecap="round" strokeLinejoin="round" d="M4 7v10c0 2.21 3.582 4 8 4s8-1.79 8-4V7M4 7c0 2.21 3.582 4 8 4s8-1.79 8-4M4 7a8 8 0 018-4s8 1.79 8 4m0 5c0 2.21-3.582 4-8 4s-8-1.79-8-4" /></svg>;
 
 
 const AdminDashboard = () => {
@@ -28,7 +26,6 @@ const AdminDashboard = () => {
             case 'bulk': return <SmartBulkBetting />;
             case 'monitor': return <LiveBettingMonitor />;
             case 'reports': return <SmartReporting />;
-            case 'backup': return <DataManagement />;
             case 'profile': return <ProfileSettings />;
             default: return null;
         }
@@ -57,7 +54,6 @@ const AdminDashboard = () => {
                     <TabButton tabId="bulk" icon={<BulkIcon />}>Bulk Betting</TabButton>
                     <TabButton tabId="monitor" icon={<MonitorIcon />}>Live Betting</TabButton>
                     <TabButton tabId="reports" icon={<ReportIcon />}>Reporting</TabButton>
-                    <TabButton tabId="backup" icon={<BackupIcon />}>Backup</TabButton>
                     <TabButton tabId="profile" icon={<ProfileIcon />}>Profile</TabButton>
                 </nav>
             </div>
