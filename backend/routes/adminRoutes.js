db4369968d11aa482de6ff23351de2236f9e64de
romiali <rumialirome@gmail.com>
2025-09-15 00:50:10 +0500

    Update adminRoutes.js

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { protect, admin } = require('../middleware/authMiddleware');
+const path = require('path');
+const { protect, admin } = require(path.join(__dirname, '..', 'middleware', 'authMiddleware'));
 const { 
     getAllClients, 
     registerClient, 
@@ -14,7 +15,7 @@ const {
     placeBetsForClient,
     getAllBets,
     getAllTransactions
-} = require('../controllers/adminController');
+} = require(path.join(__dirname, '..', 'controllers', 'adminController'));
 
 // All routes in this file are protected and require admin privileges
 router.use(protect, admin);
