1be5f160a6efa70cda67c43d03b2fdb4379bedfb
romiali <rumialirome@gmail.com>
2025-09-16 00:41:59 +0500

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,21 +8,21 @@ module.exports = {
     env: {
       NODE_ENV: 'development',
       PORT: 5000,
-      JWT_SECRET: 'ddl_secret_key_for_jwt_2024_dev',
-      API_KEY: 'AIzaSyD48pI0ogdWfG86md76C4bmBlvp4XTp1Tk',
-      DB_HOST: 'localhost',
+      JWT_SECRET: 'AIzaSyCF0j0LFCwPdpz30sdfiyEHG44qlLIGW1Q?',
+      API_KEY: 'AIzaSyCF0j0LFCwPdpz30sdfiyEHG44qlLIGW1Q',
+      DB_HOST: '127.0.0.1',
       DB_USER: 'root',
-      DB_PASSWORD: 'password',
+      DB_PASSWORD: 'Imranali@Guru1',
       DB_DATABASE: 'mydb'
     },
     env_production: {
       NODE_ENV: 'production',
       PORT: 5000, // This might be different in a real production environment
-      JWT_SECRET: 'a_much_stronger_production_secret_for_ddl_app_!@#$', // This MUST be changed to a secure, random key for production
-      API_KEY: 'AIzaSyD48pI0ogdWfG86md76C4bmBlvp4XTp1Tk',
-      DB_HOST: 'localhost',
+      JWT_SECRET: 'AIzaSyCF0j0LFCwPdpz30sdfiyEHG44qlLIGW1Q?', // This MUST be changed to a secure, random key for production
+      API_KEY: 'AIzaSyCF0j0LFCwPdpz30sdfiyEHG44qlLIGW1Q',
+      DB_HOST: '127.0.0.1',
       DB_USER: 'ddl_user',
-      DB_PASSWORD: 'your_strong_password',
+      DB_PASSWORD: 'Imranali@Guru1',
       DB_DATABASE: 'mydb'
     }
   }]
