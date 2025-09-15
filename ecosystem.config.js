7d40aa6916ec1aed44e8d3d40adcd62f20e41763
romiali <rumialirome@gmail.com>
2025-09-16 01:00:21 +0500

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -18,7 +18,7 @@ module.exports = {
     env_production: {
       NODE_ENV: 'production',
       PORT: 5000, // This might be different in a real production environment
-      JWT_SECRET: 'AIzaSyCF0j0LFCwPdpz30sdfiyEHG44qlLIGW1Q?', // This MUST be changed to a secure, random key for production
+      JWT_SECRET: 'AIzaSyCF0j0LFCwPdpz30sdfiyEHG44qlLIGW1Q??', // This MUST be changed to a secure, random key for production
       API_KEY: 'AIzaSyCF0j0LFCwPdpz30sdfiyEHG44qlLIGW1Q',
       DB_HOST: '127.0.0.1',
       DB_USER: 'ddl_user',
